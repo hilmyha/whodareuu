f00c8a58c05a446b0d57b07b93ec0fdbacf05ae6
hilmyha <hilmyahmadhaidar@gmail.com>
2025-09-15 15:27:52 +0700

    feat: rss-feed feature

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+  // ambil semua postingan dari collection `posts`
+  const posts = await getCollection("posts");
+
+  return rss({
+    title: "Hilmy Haidar Blog",
+    description:
+      "I write about the web, programming, and sometimes my life experiences. Enjoy reading!",
+    site: context.site, // otomatis ambil base URL dari astro.config.mjs
+    stylesheet: "/rss/styles.xsl", // opsional, kalau mau styling di browser
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description, // pastikan di frontmatter md ada `excerpt`
+      link: `/blog/${post.slug}`,
+    })),
+  });
+}
